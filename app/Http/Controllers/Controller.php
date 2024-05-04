44e96137e16546f56bf087baba634838a5e071e0
Md Tarikul Islam <125537691+Tarikulat@users.noreply.github.com>
2024-05-04 17:44:26 +0600

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+// Controllers starting
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
